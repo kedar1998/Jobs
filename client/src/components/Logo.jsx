6848d0173f36064bd9e2039acbd3565942670864
Kedar <kedarmakode1598@gmail.com>
2023-04-02 16:44:19 +0530

    add react router

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/images/logo.svg'
+
+const Logo = () => {
+  return (
+    <div>
+        <img src={logo} className='logo' alt="LOGO" />
+    </div>
+  )
+}
+
+export default Logo
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,20 +1,21 @@
-import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
+import {Logo} from '../components'
+import main from '../assets/images/main-alternative.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
+import {Link} from 'react-router-dom'
 
 const Landing = () => {
   return (
     <Wrapper>
         <nav>
-            <img src={logo} className='logo' alt="LOGO" />
+            <Logo />
         </nav>
         <div className='container page'>
-        
+
             {/* LEFT */}
             <div className='info'>
                 <h1>job <span>tracking</span> app</h1>
                 <p>Laborum tempor non mollit sint esse ipsum esse sint est adipisicing aliquip in ut ullamco. Culpa enim adipisicing do minim aliquip elit ipsum officia commodo proident in quis. Et labore esse ipsum excepteur sint minim consectetur reprehenderit voluptate dolore amet pariatur non esse. Nulla minim pariatur ex eu commodo adipisicing aute. Irure aute nostrud consectetur sunt esse. Consequat esse cillum laborum exercitation.</p>
-                <button className='btn btn-hero'>Login/Register</button>
+                <Link to="/register" className='btn btn-hero'>Login/Register</Link>
             </div>
 
             {/* RIGHT */}
