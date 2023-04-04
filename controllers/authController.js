66ef28197f3526433778dd8d0282faa2531b8136
Kedar <kedarmakode1598@gmail.com>
2023-04-04 21:15:13 +0530

    add custom errors and error-handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,21 @@
 import User from '../models/User.js'
 import {StatusCodes} from 'http-status-codes'
+import {BadRequestError} from '../errors/index.js'
 
 const register = async (req,res,next) =>{
+
+    const {name,email,password} = req.body
+
+    if(!name || !email || !password){
+        throw new BadRequestError("Please provide all values")
+    }
+
+    const userAlreadyExist = await User.findOne({email})
+
+    if(userAlreadyExist){
+        throw new BadRequestError("Email already in use")
+    }
+
     const user = await User.create(req.body)
     res.status(StatusCodes.CREATED).json({
         user
