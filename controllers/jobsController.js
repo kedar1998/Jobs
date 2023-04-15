090febd8f99871dd28a96e0aa30a6e5fe589412c
Kedar <kedarmakode1598@gmail.com>
2023-04-15 12:32:31 +0530

    add pagination functionality in backend

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -77,11 +77,21 @@ const getAllJobs = async (req,res) =>{
         result = result.sort('-position')
     }
 
+    // PAGINATION
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1) * limit
+
+    result = result.skip(skip).limit(limit)
+
     const jobs = await result
 
+    const totalJobs = await Job.countDocuments(queryObject)
+    const numOfPages = Math.ceil(totalJobs / limit)
+
     res.status(StatusCodes.OK).json({
-        totalJobs: jobs.length,
-        numOfPages: 1,
+        totalJobs,
+        numOfPages,
         jobs,
     })
 }
